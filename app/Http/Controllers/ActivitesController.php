c3d12bc319bbd39dcaf503522adcae01d61cc503
Yassine <yassine..razaq@gmail.com>
2020-05-11 13:36:13 +0000

    new yassine commit

--- app/Http/Controllers/ActivitesController.php
+++ app/Http/Controllers/ActivitesController.php
@@ -1,27 +1,33 @@
-<?php
+
 
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Activite;
+
 class ActivitesController extends Controller
 {
-    public function index(){
-        $Activites=  Activite::all();
-        return view('Activites.index')->with('Activites',$Activites);
+    public function index()
+    {
+        $Activites = Activite::all();
+        return view('Activites.index')->with('Activites', $Activites);
     }
-    public function create(){
+
+    public function create()
+    {
         return view('Activites.create');
     }
-    public function store(Request $request){
-         $this->validate($request,[
-             'theme' => 'required' ,
-             'type' => 'required' ,
-             'resume' => 'required' ,
-             'article' => 'required'
-         ]);
-         $activite = new Activite;
-         $activite->theme = $request->input('theme');
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'theme' => 'required',
+            'type' => 'required',
+            'resume' => 'required',
+            'article' => 'required'
+        ]);
+        $activite = new Activite;
+        $activite->theme = $request->input('theme');
         $activite->Type_Activite = $request->input('type');
         $activite->resume = $request->input('resume');
         $activite->article = $request->input('article');
